206f1793838a71f89e3204dd83484eb826f91f93
TechHiveDevs <techhivedevs@gmail.com>
2022-11-23 09:16:47 +0000

    inital auto commit

---
+++ apps/client/entities/entites.ts
@@ -0,0 +1,8 @@
+
+import User from "./user/ui.user";
+import Postmario from "./postmario/ui.postmario";
+
+export default [
+User,
+Postmario,
+]
---
+++ apps/client/entities/postmario/ui.postmario.ts
@@ -0,0 +1,15 @@
+
+import show from "./show.postmario";
+import create from "./create.postmario";
+import edit from "./edit.postmario";
+import list from "./list.postmario";
+
+export default { 
+  name: "postmario",
+  label: "postmario",
+  hide: false,
+  create,
+  edit,
+  list,
+  show,
+}
